8a582c511e595e2ba7172b0d71ed905c99613283
Jose Elias Vargas L <jose.vargas1@one.verizon.com>
2018-12-27 19:31:11 -0400

    Added logic to show spinner to PaymentHistory

--- src/components/PaymentHistory.vue
+++ src/components/PaymentHistory.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
-        <h3 class="title">Payment history</h3>
+        <h3 class="title">Payment history </h3>
+        <spinner v-if="requestPending"/>
         <guava-data-table :columns="fields" :rows="payments" @edited-item="editedItem"></guava-data-table>
     </div>
 </template>
 
 <script>
 import GuavaDataTable from "./data-table/guava-data-table.vue";
+import Spinner from "./spinner.vue";
 import { FETCH_PAYMENTS, UPDATE_PAYMENT } from "../store/actions/action-types";
+import { TOGGLE_REQUEST_PENDING, SET_REQUEST_PENDING } from "../store/mutations/mutations-types";
 
 const paymentHistoryColumns = [
     {
@@ -39,24 +42,30 @@ const paymentHistoryColumns = [
 export default {
     name: 'PaymentHistory',
     components: {
-        GuavaDataTable
+        GuavaDataTable,
+        Spinner
     },
 
-    mounted() {
-        this.$store.dispatch(FETCH_PAYMENTS);
+    beforeMount() {
+        this.$store.commit(SET_REQUEST_PENDING, true);
+        this.$store.dispatch(FETCH_PAYMENTS).then(() => {
+            this.$store.commit(TOGGLE_REQUEST_PENDING);
+        });
     },
 
     computed: {
         payments() {
             return this.$store.state.payments;
+        },
+
+        requestPending() {
+            return this.$store.state.isRequestPending;
         }
     },
 
     methods: {
         editedItem(payload){
-            this.$store.dispatch(UPDATE_PAYMENT, {
-                payload
-            });
+            this.$store.dispatch(UPDATE_PAYMENT, payload);
         }
     },
 
