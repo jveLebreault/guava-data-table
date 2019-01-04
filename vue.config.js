c81a48614ab073f9112103e9c45a4827cedb1e9d
Jose Elias Vargas Lebreault <joseevargasl@gmail.com>
2019-01-04 08:38:34 -0400

    Added s3-deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 module.exports = {
     devServer: {
         proxy: 'http://localhost:8085/'
+    },
+
+    pluginOptions: {
+      s3Deploy: {
+        registry: undefined,
+        awsProfile: 'default',
+        region: 'us-east-1',
+        bucket: 'datatable-britecore',
+        createBucket: false,
+        staticHosting: true,
+        staticIndexPage: 'index.html',
+        staticErrorPage: 'index.html',
+        assetPath: 'dist',
+        assetMatch: '**',
+        deployPath: '/',
+        acl: 'public-read',
+        pwa: false,
+        enableCloudfront: false,
+        uploadConcurrency: 5,
+        pluginVersion: '3.0.0'
+      }
     }
 }
